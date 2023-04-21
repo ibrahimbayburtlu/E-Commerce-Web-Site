876119db741c2a5d72aaf0034a87a2c2f1b40d5f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-04-21 23:04:42 +0300

    Ecommerce-app

---
+++ 02-Backend/spring-boot-ecommerce/src/main/java/com/ibrahimbayburtlu/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.ibrahimbayburtlu.ecommerce.dao;
+
+import com.ibrahimbayburtlu.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+
+}
